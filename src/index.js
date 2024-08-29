abf299c7313a7ad7b948e03441df0bd074fa6782
ornops <joshuaornopia@gmail.com>
2024-08-29 14:57:44 +0800

    Added Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomePage from './pages/home-page/HomePage'
+import ProfilesPage from './pages/profiles-page/ProfilesPage';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <HomePage/>,
+  },
+  {
+    path: '/profiles',
+    element: <ProfilesPage/>,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
---
+++ src/pages/profiles-page/ProfilesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilesPage = () => {
+  return (
+    <div>
+      <h1>Profiles Page</h1>
+    </div>
+  )
+}
+
+export default ProfilesPage;
